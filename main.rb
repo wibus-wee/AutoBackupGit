59bc9ced86ea9adf7c6f24f34d30c884bfda7bec
wibus-wee <1596355173@qq.com>
2024-03-10 00:10:23 +0800

    check commit

--- main.rb
+++ main.rb
@@ -1,11 +1,17 @@
 require 'fileutils'
 require 'digest/md5'
 
+git_base_url = "https://github.com"
+
+# Git 仓库名
+repo_names = [
+  "QiuChenlyOpenSource/91QiuChen",
+  "yuzu-mirror/yuzu"
+]
+
 # Git仓库的URL数组
-repo_urls = [
-  "https://github.com/QiuChenlyOpenSource/91QiuChen.git",
-  "https://github.com/yuzu-mirror/yuzu.git"
-  ]
+repo_urls = repo_names.map { |repo_name| "#{git_base_url}/#{repo_name}.git" }
+
 # 最多备份个数
 max_backup_count = 10
 
@@ -33,13 +39,25 @@ repo_urls.each do |repo_url|
   repo_name_md5 = Digest::MD5.hexdigest(repo_name)
   timestamp = Time.now.strftime("%Y%m%d%H%M%S")
   backup_repo_dir = File.join(backup_dir, repo_name_md5, "#{timestamp}")
-
   # 如果不是开发模式，执行git clone命令
   if dev_mode
     puts "Would execute: git clone #{repo_url} #{backup_repo_dir} --depth 1"
   else
     `git clone #{repo_url} #{backup_repo_dir} --depth 1`
     puts "Backup of #{repo_url} taken in #{backup_repo_dir}"
+    # 检查最近一次 commit 的时间
+    last_commit_time = `cd #{backup_repo_dir} && git log -1 --format=%cd`
+    puts "Last commit time: #{last_commit_time}"
+    # 和当前备份文件夹下最新的备份进行比较，如果发现这几天没有新的 commit，就不进行备份
+    latest_backup_file = Dir.glob(File.join(backup_dir, "#{repo_name_md5}_*")).sort.last
+    if latest_backup_file
+      latest_backup_time = latest_backup_file.split("_").last
+      if last_commit_time <= latest_backup_time
+        puts "No new commit since last backup, skipping"
+        FileUtils.rm_rf(backup_repo_dir)
+        next
+      end
+    end
     # 删除 .git 目录
     FileUtils.rm_rf(File.join(backup_repo_dir, ".git"))
     puts "Removed .git directory from #{backup_repo_dir}"
