16195cabd5c71d83103d52f963fa52159bca8197
wibus-wee <1596355173@qq.com>
2024-03-17 08:46:06 +0800

    Update backup workflow and directory names

--- main.rb
+++ main.rb
@@ -20,19 +20,20 @@ max_backup_count = 10
 # 备份目录路径
 backup_dir = "./backup"
 # 备份分支路径
-backup_branch_dir = "./_backup"
-
+backup_branch_dir = "./backup_branch"
 # 开发模式标志
 dev_mode = false
+
+# 如果备份分支目录不存在，警告退出
+unless File.directory?(backup_branch_dir)
+  puts "[E] Backup branch directory #{backup_branch_dir} not found"
+  puts "Ruby script aborted."
+  exit
+end
 
 # 如果备份目录不存在，创建它
 unless File.directory?(backup_dir)  
   FileUtils.mkdir_p(backup_dir)
-end
-
-# 删除备份目录下的全部压缩文件
-Dir.glob(File.join(backup_dir, "*.zip")).each do |file|
-  FileUtils.rm_rf(file)
 end
 
 # 遍历所有的仓库URL
@@ -45,13 +46,20 @@ repo_urls.each do |repo_url|
   backup_repo_dir = File.join(backup_dir, repo_name_md5, "#{timestamp}")
   # 如果不是开发模式，执行git clone命令
   if dev_mode
+    puts "Current directory: #{Dir.pwd}"
     puts "Would execute: git clone #{repo_url} #{backup_repo_dir} --depth 1"
+    puts "Glob path: #{File.join(backup_branch_dir, repo_name_md5, "*")}"
+    puts "Backup file: #{Dir.glob(File.join(backup_branch_dir, repo_name_md5, "*"))}"
   else
     `git clone #{repo_url} #{backup_repo_dir} --depth 1`
     puts "Backup of #{repo_url} taken in #{backup_repo_dir}"
     # 获取最近一次备份仓库的时间（找文件）
     last_backup_file = Dir.glob(File.join(backup_branch_dir, repo_name_md5, "*")).sort.last
-    last_backup_time = File.mtime(last_backup_file)
+    if last_backup_file.nil?
+      last_backup_time = Time.at(0)
+    else
+      last_backup_time = File.mtime(last_backup_file)
+    end
     puts "Last backup time: #{last_backup_time}" # 2024-03-09 16:16:49 +0000
     # 检查最近一次 commit 的时间
     last_commit_time = `cd #{backup_repo_dir} && git log -1 --format=%cd`.chomp
